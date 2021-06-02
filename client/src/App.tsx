1f6f28cf6e083fd1c2b9b1b0be57147da3c99f17
valer13572@gmail.com <46524230+Desonancen@users.noreply.github.com>
2021-06-02 22:26:21 +0300

    Add folder structure on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Map} from './components/Map/Map';
 /**
  * @return {Object}
  */
 function App() {
-  return (
-    <div className="App">
-      <Map></Map>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
