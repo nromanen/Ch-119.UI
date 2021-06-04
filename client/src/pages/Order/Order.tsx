29fb4fd1e3c6639b4baaf7d717e095b9a39bd590
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2021-06-04 21:27:41 +0300

    Create order page

---
+++ client/src/pages/Order/Order.tsx
@@ -0,0 +1,90 @@
+import { useCallback, useState } from 'react'
+import { Map } from './Map'
+import { OrderForm } from './OrderForm'
+
+export const Order = () => {
+  const [directions, setDirections] = useState<google.maps.DirectionsRequest>()
+  const [from, setFrom] = useState('')
+  const [to, setTo] = useState('')
+  const [map, setMap] = useState<google.maps.Map>()
+
+  const [fromAutocomplete, setFromAutocomplete] = useState({
+    getPlace: () => {},
+  })
+  const [toAutocomplete, setToAutocomplete] = useState({ getPlace: () => {} })
+
+  const onFromAutocompleteLoad = (autocomplete: any): void => {
+    console.log('autocomplete: ', autocomplete)
+
+    setFromAutocomplete(autocomplete)
+  }
+  const onToAutocompleteLoad = (autocomplete: any): void => {
+    console.log('autocomplete: ', autocomplete)
+
+    setToAutocomplete(autocomplete)
+  }
+
+  const onFromChanged = (): void => {
+    if (fromAutocomplete !== null) {
+      const geometry: any = fromAutocomplete.getPlace()
+      console.log(geometry, 'from geometry')
+
+      setFrom(geometry.formatted_address)
+    } else {
+      console.log('Autocomplete is not loaded yet!')
+    }
+  }
+  const onToChanged = (): void => {
+    if (toAutocomplete !== null) {
+      const geometry: any = toAutocomplete.getPlace()
+
+      setTo(geometry.formatted_address)
+    } else {
+      console.log('Autocomplete is not loaded yet!')
+    }
+  }
+
+  const createPath = (): void => {
+    const options: google.maps.DirectionsRequest = {
+      origin: from,
+      destination: to,
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.METRIC,
+    }
+    console.log(options, 'options')
+
+    setDirections(options)
+  }
+
+  const onMapLoaded = useCallback((map: google.maps.Map) => {
+    setMap(map)
+  }, [])
+
+  const mapProrps = {
+    directions,
+    setDirections,
+    onMapLoaded,
+    map,
+    setFrom,
+    setTo,
+  }
+  const OrderFormProrps = {
+    from,
+    setFrom,
+    to,
+    setTo,
+    map,
+    onFromAutocompleteLoad,
+    onToAutocompleteLoad,
+    onFromChanged,
+    onToChanged,
+    createPath,
+  }
+
+  return (
+    <>
+      <Map {...mapProrps} />
+      <OrderForm {...OrderFormProrps} />
+    </>
+  )
+}
---
+++ client/src/pages/Order/OrderForm.tsx
@@ -0,0 +1,144 @@
+import { FC } from 'react'
+import { Accordion, Card, Badge, Form, Button } from 'react-bootstrap'
+import { Autocomplete } from '@react-google-maps/api'
+import { Jumbotron } from 'react-bootstrap'
+
+interface OrderFormProps {
+  createPath: () => void
+  onFromAutocompleteLoad: (autocomplete: any) => void
+  onToAutocompleteLoad: (autocomplete: any) => void
+  onFromChanged: () => void
+  onToChanged: () => void
+  setTo: (v: string) => void
+  setFrom: (v: string) => void
+  map?: google.maps.Map
+  from: string
+  to: string
+}
+
+export const OrderForm: FC<OrderFormProps> = ({
+  createPath,
+  onFromAutocompleteLoad,
+  onFromChanged,
+  onToAutocompleteLoad,
+  onToChanged,
+  map,
+  from,
+  to,
+  setFrom,
+  setTo,
+}) => {
+  return (
+    <Jumbotron>
+      <Form className="form">
+        <Form.Group className="form-group">
+          <Form.Label className="col-xs-2" htmlFor="from">
+            From:
+          </Form.Label>
+          <div className="col-xs-4">
+            {map && (
+              <Autocomplete
+                onLoad={onFromAutocompleteLoad}
+                onPlaceChanged={onFromChanged}
+              >
+                <input
+                  onChange={(e) => setFrom(e.target.value)}
+                  value={from}
+                  className="form-control"
+                  type="text"
+                  name="from"
+                  id="from"
+                  placeholder="Origin"
+                />
+              </Autocomplete>
+            )}
+          </div>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="col-xs-2" htmlFor="to">
+            To:
+          </Form.Label>
+          <div className="col-xs-4">
+            {map && (
+              <Autocomplete
+                onLoad={onToAutocompleteLoad}
+                onPlaceChanged={onToChanged}
+              >
+                <input
+                  value={to}
+                  onChange={(e) => setTo(e.target.value)}
+                  className="form-control"
+                  type="text"
+                  name="to"
+                  id="to"
+                  placeholder="Destination"
+                />
+              </Autocomplete>
+            )}
+          </div>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="col-xs-2" htmlFor="car-type">
+            Car type:
+          </Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="Basic"
+            id="car-type"
+            className="form-select form-control col-xs-4"
+            aria-label="Car type select"
+          >
+            <option value="Basic">Basic</option>
+            <option value="Comfort">Comfort</option>
+            <option value="Eco">Eco</option>
+            <option value="Xl">Xl</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group>
+          <Accordion defaultActiveKey="0">
+            <Accordion.Toggle as={Form.Label} variant="link" eventKey="0">
+              Extra services:
+            </Accordion.Toggle>
+            <Card>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Form.Check
+                    id="option-1"
+                    aria-label="option 1"
+                    type="checkbox"
+                    label="English speaking"
+                  />
+                  <Form.Check
+                    id="option-2"
+                    aria-label="option 2"
+                    type="checkbox"
+                    label="Silent driver"
+                  />
+                  <Form.Check
+                    id="option-3"
+                    aria-label="option 3"
+                    type="checkbox"
+                    label="Baby chair"
+                  />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Form.Group>
+
+        <Badge as="p" variant="primary">
+          $ 15
+        </Badge>
+        <div className="col-xs-offset-2 col-xs-10">
+          <Button onClick={createPath} variant="info">
+            Calculate
+          </Button>
+          {/* <button  className="btn btn-lg btn-info">
+          Calculate
+        </button> */}
+        </div>
+      </Form>
+    </Jumbotron>
+  )
+}
