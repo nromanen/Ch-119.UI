a466534327c4f82cd3f5c0c806077f32d176326b
Vasyl Sava <Vasyard@gmail.com>
2021-06-04 13:59:55 +0300

    Delete unnecessary parameter for response handling method

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,14 +4,14 @@ import * as appConstants from '../constants/app';
 
 const router = express.Router();
 
-const response = (req: Request, res: Response, code: number, data: any) => {
+const response = (res: Response, code: number, data: any) => {
   res.status(code).send({
     ...data,
   });
 };
 
 router.get('/', (req: Request, res: Response) => {
-  response(req, res, appConstants.SC_OK, { message: 'Server is running!' });
+  response(res, appConstants.SC_OK, { message: 'Server is running!' });
 });
 
 export default router;
