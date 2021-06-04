f01f71138321e49849abde0d9ebefad89376a200
Vasyl Sava <Vasyard@gmail.com>
2021-06-04 16:38:09 +0300

    Delete redundant variables in sequelize config file and in gitignore

--- server/src/db/sequelize/config/config.ts
+++ server/src/db/sequelize/config/config.ts
@@ -9,20 +9,4 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: process.env.DB_CONNECTION,
   },
-  staging: {
-    username: process.env.STAGING_DB_USERNAME,
-    password: process.env.STAGING_DB_PASSWORD,
-    database: process.env.STAGING_DB_DATABASE,
-    host: process.env.STAGING_DB_HOST,
-    port: process.env.STAGING_DB_PORT,
-    dialect: process.env.STAGING_DB_CONNECTION,
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_DATABASE,
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    dialect: process.env.PROD_DB_CONNECTION,
-  },
 };
