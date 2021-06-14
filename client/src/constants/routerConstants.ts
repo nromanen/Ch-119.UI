3e5b7dafabe17c8c33bb01f12b660e68c34121a6
Vasyl Sava <Vasyard@gmail.com>
2021-06-14 11:22:32 +0300

    Merge active-orders into feedback

--- client/src/constants/routerConstants.ts
+++ client/src/constants/routerConstants.ts
@@ -2,3 +2,5 @@ export const LOGIN_ROUTE = '/';
 export const REGISTRATION_ROUTE = '/registration';
 export const ORDER_ROUTE = '/order';
 export const FEEDBACK_ROUTE = '/:orderId/feedback';
+export const ORDER_ACTIVE_ROUTE = '/order-active';
+export const ORDER_ACCEPTED_ROUTE = '/order-accepted';
--- client/src/pages/AppRouter.tsx
+++ client/src/pages/AppRouter.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react';
-import { Switch, Route, Redirect, RouteProps } from 'react-router-dom';
-import { publicRouters } from '../routes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { authRouters, publicRouters } from '../routes';
 import { LOGIN_ROUTE } from '../constants/routerConstants';
 
 const AppRouter: FC = () => {
   return (
     <Switch>
-      {publicRouters.map((route: RouteProps) => (
+      {publicRouters.map(({ path, component }) => (
+        <Route key={path?.toString()} path={path} component={component} exact />
+      ))},
+      {authRouters.map(({ path, component }, index) => (
         <Route
-          key={route.path?.toString()}
-          path={route.path}
-          component={route.component}
-        />
-      ))}
+          key={path?.toString()} path={path} component={component} exact />
+      ))},
       <Redirect to={LOGIN_ROUTE} />
     </Switch>
   );
