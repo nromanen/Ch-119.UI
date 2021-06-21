d83011d26349f8f1d84f47e0185ed5cfcaa9e3da
valer13572@gmail.com <46524230+Desonancen@users.noreply.github.com>
2021-06-21 18:00:23 +0300

    Refactored validators

--- client/src/utils/formValidators.ts
+++ client/src/utils/formValidators.ts
@@ -1,6 +1,6 @@
 import { REQUIRED, MATCH_PASSWORDS } from '../constants/validatorsConstants';
 
-export const required = (value: string) => (value ? undefined : 'Required');
+export const required = (value: string) => (value ? undefined : REQUIRED);
 
 export const confirmPassword = (value: any) => {
   if (value.confirm !== value.password) return MATCH_PASSWORDS;
