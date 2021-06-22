464b4331a023b4c164b9e51219f7066b044794d9
valer13572@gmail.com <46524230+Desonancen@users.noreply.github.com>
2021-06-22 18:38:09 +0300

    Logged as Driver

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -10,9 +10,9 @@ const Profile: FC = (props: any) =>
         <p><strong>Username:</strong> {props.auth.name}</p>
         <p><strong>Phone:</strong> {props.auth.phone}</p>
         {props.auth.isDriver ? (<React.Fragment>
-        <p><strong>Car color:</strong>{props.auth.driver_info.car_color}</p>
-        <p><strong>Car model:</strong>{props.auth.driver_info.car_model}</p>
-        <p><strong>Car number:</strong>{props.auth.driver_info.car_number}</p>
+        <p><strong>Car color:</strong> {props.auth.driver_info.car_color}</p>
+        <p><strong>Car model:</strong> {props.auth.driver_info.car_model}</p>
+        <p><strong>Car number:</strong> {props.auth.driver_info.car_number}</p>
         </React.Fragment>): null}
         <button className='btn btn-danger' onClick={props.logoutUser}>Log out</button>
       </div>
